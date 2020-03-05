08b8564fd61b2abad0559537d30727648c97f64d
deemch3k <smokeers3@gmail.com>
2020-03-06 00:04:08 +0100

    -adding Open Graph Protocol and Twitter card parser

--- src/main/java/dima/sarafan/domain/Message.java
+++ src/main/java/dima/sarafan/domain/Message.java
@@ -3,6 +3,7 @@ package dima.sarafan.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -27,13 +28,17 @@ public class Message {
     @JsonView(Views.FullMessage.class)
     private LocalDateTime creationDate;
 
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String link;
 
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
+    @JsonView(Views.FullMessage.class)
+    private String linkTitle;
+
+    @JsonView(Views.FullMessage.class)
+    private String linkDescription;
+
+    @JsonView(Views.FullMessage.class)
+    private String linkCover;
 
     public Long getId() {
         return id;
@@ -49,5 +54,45 @@ public class Message {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getLinkTitle() {
+        return linkTitle;
+    }
+
+    public void setLinkTitle(String linkTitle) {
+        this.linkTitle = linkTitle;
+    }
+
+    public String getLinkDescription() {
+        return linkDescription;
+    }
+
+    public void setLinkDescription(String linkDescription) {
+        this.linkDescription = linkDescription;
+    }
+
+    public String getLinkCover() {
+        return linkCover;
+    }
+
+    public void setLinkCover(String linkCover) {
+        this.linkCover = linkCover;
     }
 }
