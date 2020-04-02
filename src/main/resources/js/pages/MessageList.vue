689a9a1fcf66ee0e84eb6d9f08ffed43192d54de
deemch3k <smokeers3@gmail.com>
2020-04-03 00:13:10 +0200

    -deploy on heroku

--- src/main/resources/js/pages/MessageList.vue
+++ src/main/resources/js/pages/MessageList.vue
@@ -13,10 +13,9 @@
 
 <script>
     import {mapGetters} from 'vuex'
-    import MessageRow from 'components/messages/MessageRow.vue'
-    import MessageForm from 'components/messages/MessageForm.vue'
+    import MessageRow from "../components/messages/MessageRow.vue";
+    import MessageForm from "../components/messages/MessageForm.vue";
     import LazyLoader from "../components/LazyLoader.vue";
-
     export default {
         components: {
             LazyLoader,
